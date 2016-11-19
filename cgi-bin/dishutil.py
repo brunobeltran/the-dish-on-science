5fc5c2e7b4ebf44ae6715e346ec71cda605c914a
Bruno Beltran <brunobeltran0@gmail.com>
2016-11-18 17:38:04 -0800

    CODE: update view_count using xlsx
    Former-commit-id: ee213d07d2ec5b6880a391445271aec5bd542b27

---
+++ cgi-bin/dishutil.py
@@ -0,0 +1,34 @@
+import xlrd
+import dishsql
+import re
+
+
+def update_counts_manually(file_name):
+    """Reads an excel file whose first column (A) is the page name and
+    second column (B) is the number of uncounted views to increment the sql
+    table's counters by."""
+    wb = xlrd.open_workbook(file_name)
+    sh = wb.sheet_by_index(0)
+    pages = sh.col(0)
+    counts = sh.col(1)
+    # for now, we only keep track of post page view counts, so we can ignore
+    # everything else
+    extract_post_name = re.compile('.*/posts/([-a-zA-Z0-9]*)/?')
+    to_update = dict()
+    for i, page in enumerate(pages):
+        match = extract_post_name.match(page.value)
+        if match:
+            to_update[match.groups()[0]] = counts[i].value
+
+    with dishsql.session_scope() as session:
+        for page, count in to_update.items():
+            post = session.query(dishsql.Post).\
+                           filter_by(url_title=page).\
+                           first()
+            if post:
+                post.view_count = post.view_count + count
+
+
+
+
+
